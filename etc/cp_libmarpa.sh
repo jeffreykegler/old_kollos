b5b404ffe544f71d6d71e8ace1d43092d56d7810
Jeffrey Kegler <JKEGL@cpan.org>
2015-01-15 20:09:02 -0800

    Packaging

---
+++ etc/cp_libmarpa.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+# Copyright 2015 Jeffrey Kegler
+# This file is part of Libmarpa.  Libmarpa is free software: you can
+# redistribute it and/or modify it under the terms of the GNU Lesser
+# General Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+#
+# Libmarpa is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser
+# General Public License along with Libmarpa.  If not, see
+# http://www.gnu.org/licenses/.
+
+# Copy the libmarpa distribution to a directory
+# This script assumes it is run as part of the Kollos
+# build environment.
+
+if [ $# != 1 ] 
+then
+	echo "usage: $0 target-directory" 1>&2
+	exit 1
+fi
+todir="$1"
+fromdir=cm_dist
+needtobe="you need to be in a Kollos build environment top-level directory to run this command"
+if [ ! -e "$fromdir" ]
+then
+    echo "cannot find directory $fromdir -- $needtobe" 1>&2
+    exit 1
+fi
+if [ ! -d "$fromdir" ]
+then
+    echo "$fromdir is not a directory -- $needtobe" 1>&2
+    exit 1
+fi
+checkfile="$fromdir/configure.ac"
+if [ ! -e "$checkfile" ]
+then
+    echo "$fromdir does not contain a configure.ac file -- $needtobe" 1>&2
+    exit 1
+fi
+if [ ! -r "$checkfile" ]
+then
+    echo "You not have the sufficient permissions to copy $checkfile" 1>&2
+    exit 1
+fi
+if [ -e "$todir" ]
+then
+    echo "cannot create directory $todir -- something with that name already exists" 1>&2
+    exit 1
+fi
+mkdir "$todir" || exit 1
+# Copy using copying user's permissions and new modification times
+(cd "$fromdir"; tar cf - . ) | (cd "$todir"; tar xfom -)
---
+++ etc/libmarpa_get.pl
@@ -0,0 +1,83 @@
+#!/usr/bin/perl
+# Copyright 2015 Jeffrey Kegler
+# This file is part of Marpa::R2.  Marpa::R2 is free software: you can
+# redistribute it and/or modify it under the terms of the GNU Lesser
+# General Public License as published by the Free Software Foundation,
+# either version 3 of the License, or (at your option) any later version.
+#
+# Marpa::R2 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser
+# General Public License along with Marpa::R2.  If not, see
+# http://www.gnu.org/licenses/.
+
+use 5.010;
+use strict;
+use warnings;
+use autodie;
+use IPC::Cmd;
+use File::Path;
+
+my $libmarpa_repo = 'git@github.com:jeffreykegler/libmarpa.git';
+my $stage = 'libmarpa_stage';
+
+for my $dir_to_clean ($stage, 'libmarpa_build')
+{
+  my $deleted_count = File::Path::remove_tree('../read_only');
+  say "$deleted_count files deleted in ../read_only";
+}
+
+if (not IPC::Cmd::run(
+        command => [ qw(git clone -b kollos --depth 5), $libmarpa_repo, $stage ],
+        verbose => 1
+    )
+    )
+{
+    die "Could not clone";
+} ## end if ( not IPC::Cmd::run( command => [ qw(git clone -n --depth 1)...]))
+
+# CHIDR into staging dir
+chdir $stage || die "Could not chdir";
+
+my $log_data;
+if (not IPC::Cmd::run(
+        command => [ qw(git log -n 5) ],
+        verbose => 1,
+	buffer => \$log_data
+    )
+    )
+{
+    die "Could not clone";
+} ## end if ( not IPC::Cmd::run( command => [ qw(git clone -n --depth 1)...]))
+
+{
+   open my $fh, q{>}, '../LOG_DATA';
+   print {$fh} $log_data;
+   close $fh;
+}
+
+if (not IPC::Cmd::run(
+        command => [ qw(make cm_dist) ],
+        verbose => 1
+    )
+    )
+{
+    die qq{Could not make dist};
+} ## end if ( not IPC::Cmd::run( command => [ qw(git checkout)...]))
+
+my $deleted_count = File::Path::remove_tree('../read_only');
+say "$deleted_count files deleted in ../read_only";
+
+if (not IPC::Cmd::run(
+        command => [ qw(sh etc/cp_libmarpa.sh "$stage/cm_dist" components/libmarpa) ],
+        verbose => 1
+    )
+    )
+{
+    die qq{Could not copy cm_dist};
+}
+
+exit 0
