535acb17c708eeb32870e881d06ef27f82cb56cd
Jeffrey Kegler <JKEGL@cpan.org>
2015-05-24 20:23:14 -0700

    Rearranging kollos Lua directory structure

--- components/test/json.lua
+++ components/test/json.lua
@@ -49,7 +49,7 @@ local function here() return -- luacheck: ignore here
   debug.getinfo(2,'S').source .. debug.getinfo(2, 'l').currentline
 end
 
-local dumper = require "dumper" -- luacheck: ignore
+local dumper = require "kollos.dumper" -- luacheck: ignore
 
 -- eventually most of this code becomes part of kollos
 -- for now we bring the already written part in as a
