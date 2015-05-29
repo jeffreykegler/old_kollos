0415adaa96d4b9fd32171ce5b7c2fafdf9235b0a
Jeffrey Kegler <JKEGL@cpan.org>
2015-05-28 21:40:23 -0700

    Work on JSON lexer

--- components/main/kollos.c.lua
+++ components/main/kollos.c.lua
@@ -12,6 +12,7 @@ methods allows the elimination of thousands of lines of code.
 
 -- assumes that, when called, out_file to set to output file
 local error_file
+local event_file
 
 local function c_safe_string (s)
     s = string.gsub(s, '"', '\\034')
@@ -25,6 +26,7 @@ for _,v in ipairs(arg) do
    local id, val = v:match("^([^=]+)%=(.*)") -- no space around =
    if id == "out" then io.output(val)
    elseif id == "errors" then error_file = val
+   elseif id == "events" then event_file = val
    else return nil, "Bad id in options: ", id end
 end
 
@@ -367,6 +369,129 @@ static inline int l_error_name_by_code(lua_State* L)
    return 1;
 }
  
+]=]
+
+-- event codes
+
+io.write[=[
+
+struct s_libmarpa_event_code {
+   lua_Integer code;
+   const char* mnemonic;
+   const char* description;
+};
+
+]=]
+
+do
+    local f = assert(io.open(event_file, "r"))
+    local code_lines = {}
+    local code_mnemonics = {}
+    local max_code = 0
+    while true do
+        local line = f:read()
+        if line == nil then break end
+        local i,_ = string.find(line, "#")
+        local stripped
+        if (i == nil) then stripped = line
+        else stripped = string.sub(line, 0, i-1)
+        end
+        if string.find(stripped, "%S") then
+            local raw_code
+            local raw_mnemonic
+            local description
+            _, _, raw_code, raw_mnemonic, description = string.find(stripped, "^(%d+)%sMARPA_EVENT_(%S+)%s(.*)$")
+            local code = tonumber(raw_code)
+            if description == nil then return nil, "Bad line in event code file ", line end
+            if code > max_code then max_code = code end
+            local mnemonic = 'LIBMARPA_EVENT_' .. raw_mnemonic
+            code_mnemonics[code] = mnemonic
+            code_lines[code] = string.format( '   { %d, %s, %s },',
+                code,
+                c_safe_string(mnemonic),
+                c_safe_string(description)
+                )
+        end
+    end
+
+    io.write('#define LIBMARPA_MIN_EVENT_CODE 0\n')
+    io.write('#define LIBMARPA_MAX_EVENT_CODE ' .. max_code .. '\n\n')
+
+    for i = 0, max_code do
+        local mnemonic = code_mnemonics[i]
+        if mnemonic then
+            io.write(
+                   string.format('#define %s %d\n', mnemonic, i)
+           )
+       end
+    end
+    io.write('\n')
+    io.write('struct s_libmarpa_event_code libmarpa_event_codes[LIBMARPA_MAX_EVENT_CODE-LIBMARPA_MIN_EVENT_CODE+1] = {\n')
+    for i = 0, max_code do
+        local code_line = code_lines[i]
+        if code_line then
+           io.write(code_line .. '\n')
+        else
+           io.write(
+               string.format(
+                   '    { %d, "LUIF_EVENT_RESERVED", "Unknown Libmarpa event %d" },\n',
+                   i, i
+               )
+           )
+        end
+    end
+    io.write('};\n\n');
+    f:close()
+end
+
+io.write[=[
+
+static inline const char* event_description_by_code(lua_Integer event_code)
+{
+   if (event_code >= LIBMARPA_MIN_EVENT_CODE && event_code <= LIBMARPA_MAX_EVENT_CODE) {
+       return libmarpa_event_codes[event_code-LIBMARPA_MIN_EVENT_CODE].description;
+   }
+   return (const char *)0;
+}
+
+static inline int l_event_description_by_code(lua_State* L)
+{
+   const lua_Integer event_code = luaL_checkinteger(L, 1);
+   const char* description = event_description_by_code(event_code);
+   if (description)
+   {
+       lua_pushstring(L, description);
+   } else {
+       lua_pushfstring(L, "Unknown event code (%d)", event_code);
+   }
+   return 1;
+}
+ 
+static inline const char* event_name_by_code(lua_Integer event_code)
+{
+   if (event_code >= LIBMARPA_MIN_EVENT_CODE && event_code <= LIBMARPA_MAX_EVENT_CODE) {
+       return libmarpa_event_codes[event_code-LIBMARPA_MIN_EVENT_CODE].mnemonic;
+   }
+   return (const char *)0;
+}
+
+static inline int l_event_name_by_code(lua_State* L)
+{
+   const lua_Integer event_code = luaL_checkinteger(L, 1);
+   const char* mnemonic = event_name_by_code(event_code);
+   if (mnemonic)
+   {
+       lua_pushstring(L, mnemonic);
+   } else {
+       lua_pushfstring(L, "Unknown event code (%d)", event_code);
+   }
+   return 1;
+}
+ 
+]=]
+
+io.write[=[
+
 /* userdata metatable keys
    The contents of these locations are never examined.
    These location are used as a key in the Lua registry.
@@ -1312,6 +1437,16 @@ LUALIB_API int luaopen_kollos_c(lua_State *L)
     lua_pushcfunction(L, l_error_description_by_code);
     /* [ kollos, function ] */
     lua_setfield(L, kollos_table_stack_ix, "error_description");
+    /* [ kollos ] */
+
+    lua_pushcfunction(L, l_event_name_by_code);
+    /* [ kollos, function ] */
+    lua_setfield(L, kollos_table_stack_ix, "event_name");
+    /* [ kollos ] */
+
+    lua_pushcfunction(L, l_event_description_by_code);
+    /* [ kollos, function ] */
+    lua_setfield(L, kollos_table_stack_ix, "event_description");
     /* [ kollos ] */
 
     lua_pushcfunction(L, wrap_kollos_throw);
