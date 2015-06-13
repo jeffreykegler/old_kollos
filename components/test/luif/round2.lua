93f66c225327ce3df2d42da09589e792751df0f7
Jeffrey Kegler <JKEGL@cpan.org>
2015-06-12 20:47:45 -0700

    Next attempt at LUIF

--- components/test/luif/round2.lua
+++ components/test/luif/round2.lua
@@ -1,23 +1,25 @@
-# Copyright 2015 Jeffrey Kegler
-# Permission is hereby granted, free of charge, to any person obtaining a
-# copy of this software and associated documentation files (the "Software"),
-# to deal in the Software without restriction, including without limitation
-# the rights to use, copy, modify, merge, publish, distribute, sublicense,
-# and/or sell copies of the Software, and to permit persons to whom the
-# Software is furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included
-# in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-# OTHER DEALINGS IN THE SOFTWARE.
+--[[
+Copyright 2015 Jeffrey Kegler
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
 
-# Prototype the LUIF parser
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+--]]
+
+-- Prototype the LUIF parser
 
 K = require 'kollos'
 
@@ -30,7 +32,7 @@ l0:precedence{}
 l0:alternative{'E',
    'E',
    l0:seq{'ws', min = 0, max = 1 },
-   l0:string'*'
+   l0:string'*',
    l0:seq{'ws', min = 0, max = 1 },
    'E',
    exp = 'E*E'}
@@ -38,7 +40,7 @@ l0:precedence{}
 l0:alternative{'E',
    'E',
    l0:seq{'ws', min = 0, max = 1 },
-   l0:string'+'
+   l0:string'+',
    l0:seq{'ws', min = 0, max = 1 },
    'E',
    exp = 'E+E'}
