d789f51212fb6a313bc19aadfc85f2b701d275f4
Jeffrey Kegler <JKEGL@cpan.org>
2015-06-14 18:00:54 -0700

    Work on pure Lua IF

--- components/main/kollos/grammar.lua
+++ components/main/kollos/grammar.lua
@@ -23,7 +23,7 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 --]]
 
--- Kollos top-level config objects
+-- Kollos top level grammar routines
 
 -- luacheck: std lua51
 -- luacheck: globals bit
@@ -33,41 +33,23 @@ local function here() return -- luacheck: ignore here
 end
 
 local inspect = require "kollos.inspect" -- luacheck: ignore
-local grammar = require "kollos.grammar"
-local kollos_c = require "kollos_c"
-local error_throw = kollos_c.error_throw
 
-local config_class = {
-    grammar_new = grammar._config_grammar_new
-}
+-- this will actually become a method of the config object
+local function _config_grammar_new(config, interface)
+    return {
+       xrule = {},
+       xsym = {},
+       xalt = {},
+       wsym = {},
+       wrule = {},
+    }
+end
 
-local function config_new(kollos, args)
-    local config_object = { _type = "config" }
-    -- 'alpha' means anything is OK
-    -- it is the only acceptable if, at this point
-    local throw = args.throw or true
-    -- config_object.throw = throw
-    -- 'if' is also a keyword, so it must be used in quoted form
-    if type(args['if']) ~= 'string' then
-        if throw then
-            error_throw(MARPA_ERR_DEVELOPMENT, [["if" argument is required and must be string]])
-        end
-        return
-    end
-    if args['if'] ~= 'alpha' then
-        if throw then
-            error_throw(MARPA_ERR_DEVELOPMENT, [["if = 'alpha'" is required]])
-        end
-        return
-    end
-    setmetatable(config_object, {
-            __index = config_class,
-        })
-    return config_object
+local function alternative(grammar, args)
 end
 
 return {
-    new = config_new,
+    alternative = alternative,
 }
 
 -- vim: expandtab shiftwidth=4:
