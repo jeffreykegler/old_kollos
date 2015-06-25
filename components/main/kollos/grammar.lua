0e14e1822a38f58837053298701cafab972b42f7
Jeffrey Kegler <JKEGL@cpan.org>
2015-06-24 22:01:26 -0700

    Work on pure Lua IF

--- components/main/kollos/grammar.lua
+++ components/main/kollos/grammar.lua
@@ -839,33 +839,33 @@ function grammar_class.compile(grammar, args)
         end
     end
 
--- Ban nullable prececedenced rules.
--- They are just too confusing. Tf the user
--- *really* is sure they want it, and that she
--- knows what she is doing, she can write it out
--- in BNF.
-local xrule_by_id = grammar.xrule_by_id
-for xrule_id = 1,#xrule_by_id do
-    local xrule = xrule_by_id[xrule_id]
-    local precedences = xrule.precedences
-    -- If it is a rule with multiple precedences
-    if #precedences > 1 then
-        local lhs = xrule.lhs
-        if lhs.nullable then
-            return report_nullable_precedenced_xrule(grammar, xrule)
-        end
-        -- label the top alternatives with their precedence level
-        for prec_ix = 1, #precedences do
-            local xprec = precedences[prec_ix]
-            local level = xprec.level
-            local alternatives = xprec.top_alternatives
-            for alt_ix = 1, #alternatives do
-                local alternative = alternatives[alt_ix]
-                alternative.precedence_level = level
+    -- Ban nullable prececedenced rules.
+    -- They are just too confusing. Tf the user
+    -- *really* is sure they want it, and that she
+    -- knows what she is doing, she can write it out
+    -- in BNF.
+    local xrule_by_id = grammar.xrule_by_id
+    for xrule_id = 1,#xrule_by_id do
+        local xrule = xrule_by_id[xrule_id]
+        local precedences = xrule.precedences
+        -- If it is a rule with multiple precedences
+        if #precedences > 1 then
+            local lhs = xrule.lhs
+            if lhs.nullable then
+                return report_nullable_precedenced_xrule(grammar, xrule)
+            end
+            -- label the top alternatives with their precedence level
+            for prec_ix = 1, #precedences do
+                local xprec = precedences[prec_ix]
+                local level = xprec.level
+                local alternatives = xprec.top_alternatives
+                for alt_ix = 1, #alternatives do
+                    local alternative = alternatives[alt_ix]
+                    alternative.precedence_level = level
+                end
             end
         end
     end
-end
 
     -- A sequence may be nullable because
     -- 1) min=0, or
